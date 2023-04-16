61503e22227afb0226f851f46beb1ca1b87e4cb3
elite2012 <liu_wei75@hotmail.com>
2023-04-16 16:06:18 +0800

    init

---
+++ antd-demo/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import buttons from './componet/buttons';
+
+ReactDOM.render(
+   <buttons/>,
+   document.getElementById('root')
+)
+
